f2f24765b47dde0b53a7987f1eda36872958b019
william-c-fenton <wcfenton12@gmail.com>
2019-11-14 18:31:28 -0500

    implemented combineHeaps and printHeaps

--- TypedHeap.cpp
+++ TypedHeap.cpp
@@ -4,11 +4,21 @@ void TypedHeap::insertToHeaps(vector<string> vec){
 }
 
 void TypedHeap::combineHeaps(){
+  cout << "Merging Heaps: " << endl;
 
+  totalHeap.skewHeapMerge(stringHeap);
+  totalHeap.skewHeapMerge(intHeap);
 }
 
 void TypedHeap::printHeaps() const{
+  cout << "Int Heap: " << endl;
+  intHeap.dump();
 
+  cout << "String Heap: " << endl;
+  stringHeap.dump();
+
+  cout << "Total Heap: " << endl;
+  totalHeap.dump();
 }
 
 void TypedHeap::changePriority(pri_fn pri){
